64af467218d4d7bec574d4e1658b9ae87a29c0d8
gajdafilip <filip.gajda7@interia.pl>
2023-02-08 14:26:53 +0100

    Zadanie domowe z dnia 4

--- ZadanieDomoweDzien4/Program.cs
+++ ZadanieDomoweDzien4/Program.cs
@@ -1,36 +1,33 @@
 ﻿var name = "Ewa";
-string gender = "Kobieta";
-string gender2 = "Mężczyzna";
-int Age = 28;
+bool Woman = true;
+int Age = 17;
 int Age2 = 33;
-int Age3 = 17;
 
-if (gender == "Kobieta" && Age < 30)
+if (Woman == true && Age < 30)
 {
     Console.WriteLine("Kobieta poniżej 30 lat");
 }
-if (name == "Ewa" && Age2 == 33)
+else if (name == "Ewa" && Age2 == 33)
 {
     Console.WriteLine("Ewa, lat 33");
 }
-if (name == "Ewa")
-    if (Age < 33)
-    {
-        Console.WriteLine("Ewa, poniżej 33 lat");
-    }
-if (gender2 == "Mężczyzna" && Age3 < 18)
+else if (name == "Ewa" && Age < 33)
+{
+    Console.WriteLine("Ewa, poniżej 33 lat");
+}
+else if (Woman == false && Age < 18)
 {
     Console.WriteLine("Niepełnoletni Mężczyzna");
 }
-if (gender2 == "Mężczyzna" && Age2 >= 18)
+else if (Woman == false && Age2 >= 18)
 {
     Console.WriteLine("Pełnoletni Mężczyzna");
 }
-if (gender2 != gender && Age >= 18)
+else if (Woman == true && Age2 >= 18)
 {
     Console.WriteLine("Jest to pełnoletnia kobieta");
 }
 else
 {
-    Console.WriteLine("Jestem kimś innym");
+    Console.WriteLine("Jest to ktoś inny");
 }
